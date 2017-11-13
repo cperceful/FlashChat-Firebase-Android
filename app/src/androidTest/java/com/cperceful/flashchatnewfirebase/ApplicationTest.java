94f4ba14c2269007a1be4fef738261b5222fbc75
cperceful <cpercef1@gmail.com>
2017-11-13 10:40:59 -0600

    Added firebase dependencies

--- app/src/androidTest/java/com/cperceful/flashchatnewfirebase/ApplicationTest.java
+++ app/src/androidTest/java/com/cperceful/flashchatnewfirebase/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.cperceful.flashchatnewfirebase;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/cperceful/flashchatnewfirebase/ExampleUnitTest.java
+++ app/src/test/java/com/cperceful/flashchatnewfirebase/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.cperceful.flashchatnewfirebase;
 
 import org.junit.Test;
 
